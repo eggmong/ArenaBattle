406963c41ab60db990200ccfce26970ce2f64cf4
jslee <102969527+eggmong@users.noreply.github.com>
2024-01-10 17:59:22 +0900

    애니메이션 세팅 작업중

---
+++ Source/UnrealArenaBattle/Animation/ABAnimInstance.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/ABAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UABAnimInstance::UABAnimInstance()
+{
+	MovingThreshould = 3.0f;
+	JumpingThreshould = 100.0f;
+
+
+}
+
+void UABAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// Actor 타입으로 반환되기 때문에 캐릭터인지 알 수 없음. 그래서 형변화
+	Owner = Cast<ACharacter>(GetOwningActor());
+
+	if (Owner)
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+void UABAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement)
+	{
+		Velocity = Movement->Velocity;
+		GroundSpeed = Velocity.Size2D();	// 지면의 속력은 Velocity값에서 z값을 제외한, xy의 크기만 구해서 지면의 속력을 구하면 됨
+		bIsIdle = GroundSpeed < MovingThreshould;
+		bIsFalling = Movement->IsFalling();
+		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshould);
+	}
+}
