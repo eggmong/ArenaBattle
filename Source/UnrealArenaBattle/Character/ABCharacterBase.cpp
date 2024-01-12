495393041aee4e003b615fa2e2c1a0a5c790847e
jslee <102969527+eggmong@users.noreply.github.com>
2024-01-12 14:08:15 +0900

    콤보 몽타주, 코드 구현

--- Source/UnrealArenaBattle/Character/ABCharacterBase.cpp
+++ Source/UnrealArenaBattle/Character/ABCharacterBase.cpp
@@ -7,6 +7,7 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Character/ABCharacterControlData.h"
 #include "Animation/AnimMontage.h"
+#include "ABComboActionData.h"
 
 // Sets default values
 AABCharacterBase::AABCharacterBase()
@@ -74,6 +75,104 @@ void AABCharacterBase::SetCharacterControlData(const UABCharacterControlData* Ch
 
 void AABCharacterBase::ProcessComboCommand()
 {
+    if (CurrentCombo == 0)
+    {
+        ComboActionBegin();
+        return;
+    }
+
+    // 만약 타이머가 설정 안되어 있을 때 입력이 들어왔다면
+    if (!ComboTimerHandle.IsValid())
+    {
+        // 이미 타이머가 발동 되어서 시기를 놓쳤거나
+        // 더이상 타이머 진행할 필요가 없다는 것
+        HasNextComboCommand = false;
+    }
+    else
+    {
+        // 타이머가 유효하다면
+        // 다음 섹션으로 이동시킬 커맨드가 발동했다는 뜻
+        HasNextComboCommand = true;
+    }
+}
+
+void AABCharacterBase::ComboActionBegin()
+{
+    // Combo Status
+    CurrentCombo = 1;
+
+    // Movement Setting
+    GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+
+    // Animation Setting
+    const float AttackSpeedRate = 1.f;
     UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
-    AnimInstance->Montage_Play(ComboActionMontage);
+    AnimInstance->Montage_Play(ComboActionMontage, AttackSpeedRate);
+
+    // 몽타주가 실행되면, 몽타주가 종료될 때 ComboActionEnd 함수 호출이 바로 되도록 여기서 몽타주 세팅
+    // 구조체처럼 EndDelegate 라고 선언하고, 정보를 넣을 수 있음
+    FOnMontageEnded EndDelegate;
+    // 바인딩할 오브젝트 정보 추가 (현재 인스턴스, ComboActionEnd 함수를 매핑)
+    EndDelegate.BindUObject(this, &AABCharacterBase::ComboActionEnd);
+
+    AnimInstance->Montage_SetEndDelegate(EndDelegate, ComboActionMontage);
+
+    ComboTimerHandle.Invalidate();      // 일단 무효하도록 초기화
+    SetComboCheckTimer();               // 그리고 타이머 세팅
+}
+
+void AABCharacterBase::ComboActionEnd(UAnimMontage* TargetMontage, bool IsProperlyEnded)
+{
+    // 진짜 CurrentCombo 0이 아닌지 체크 (콤보가 시작하면 무조건 1부터 시작하니까)
+    ensure(CurrentCombo != 0);
+
+    CurrentCombo = 0;
+
+    GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+}
+
+void AABCharacterBase::SetComboCheckTimer()
+{
+    int32 ComboIndex = CurrentCombo - 1;
+
+    // 각 프레임 정보가 ComboIndex와 유효한지 검사
+    ensure(ComboActionData->EffectiveFrameCount.IsValidIndex(ComboIndex));
+
+    const float AttackSpeedRate = 1.f;
+
+    // 각 액션마다 콤보가 발동할 시간 정보를 얻을 수 있음 
+    float ComboEffectiveTime = (ComboActionData->EffectiveFrameCount[ComboIndex] / ComboActionData->FrameRate) / AttackSpeedRate;
+
+    // 값이 0보다 작으면 콤보가 발동하지 않음 (마지막 콤보일 수도 있고 뭐.. 마지막엔 ComboAttack -1로 넣어줬었다! UABComboActionData 참고)
+    if (ComboEffectiveTime > 0.0f)
+    {
+        // 타이머 설정
+        GetWorld()->GetTimerManager().SetTimer(ComboTimerHandle, this, &AABCharacterBase::ComboCheck, ComboEffectiveTime, false);
+    }
+}
+
+void AABCharacterBase::ComboCheck()
+{
+    // 초기화 (타이머 또 발동되면 안되니까)
+    ComboTimerHandle.Invalidate();
+
+    // 타이머 발동 전에 입력이 들어왔다면
+    if (HasNextComboCommand)
+    {
+        UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+
+        // MaxComboCount 값을 벗어나지 않도록 제한하면서 콤보카운트 +1
+        CurrentCombo = FMath::Clamp(CurrentCombo + 1, 1, ComboActionData->MaxComboCount);
+
+        // ComboAttack1, 2, 3...
+        FName NextSection = *FString::Printf(TEXT("%s%d"), *ComboActionData->MontageSectionNamePrefix, CurrentCombo);
+
+        // 해당 몽타주 섹션으로 점프
+        AnimInstance->Montage_JumpToSection(NextSection, ComboActionMontage);
+
+        // 그리고 다시 콤보타이머 걸어주기
+        SetComboCheckTimer();
+
+        HasNextComboCommand = false;
+    }
 }
--- Source/UnrealArenaBattle/Character/ABCharacterBase.h
+++ Source/UnrealArenaBattle/Character/ABCharacterBase.h
@@ -33,8 +33,38 @@ protected:
 protected:
 	// Combo Action Section (for 몽타주)
 
+	// 몽타주 만든거 불러오려고 만든 변수 (AM_ComboAttack)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation)
 	TObjectPtr<class UAnimMontage> ComboActionMontage;
 
+	// 데이터에셋 만든거 불러오려고 만든 변수 (ABA_ComboAttack)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Attack, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UABComboActionData> ComboActionData;
+
 	void ProcessComboCommand();
+
+	// 몽타주가 시작될 때 호출하는 함수
+	void ComboActionBegin();
+
+	// 몽타주가 끝날 때 호출, 얘는 몽타주에 설정된 델리게이트를 통해 호출되도록 파라미터 줬음
+	// UAnimMontage 클래스 들어가보면 DECLARE_DELEGATE_TwoParams( FOnMontageEnded, class UAnimMontage*, bool /*bInterrupted*/) 
+	// 저거랑 똑같이 맞춘것
+	void ComboActionEnd(class UAnimMontage* TargetMontage, bool IsProperlyEnded);
+
+	// 타이머를 발동 시킬 함수
+	void SetComboCheckTimer();
+
+	// 타이머가 발동 되면 입력이 들어왔는지 체크하는 함수
+	void ComboCheck();
+
+
+
+	// 현재 콤보가 어디까지 진행됐는지 파악하려고
+	int32 CurrentCombo = 0;
+	
+	// 언리얼 엔진에서 제공하는 타이머 함수
+	FTimerHandle ComboTimerHandle;
+
+	// 타이머 이전에 입력 커맨드가 들어왔는지 체크
+	bool HasNextComboCommand = false;
 };
