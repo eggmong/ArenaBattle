78343f6ce4eb2195fe9558d6b5153ef00b685d6f
jslee <102969527+eggmong@users.noreply.github.com>
2024-01-22 17:07:37 +0900

    HUD 구현 준비 (WidgetHpBar, Stat UI 에셋 생성 및 코드 추가)

--- Source/UnrealArenaBattle/Player/ABPlayerController.cpp
+++ Source/UnrealArenaBattle/Player/ABPlayerController.cpp
@@ -2,6 +2,16 @@
 
 
 #include "Player/ABPlayerController.h"
+#include "UI/ABHUDWidget.h"
+
+AABPlayerController::AABPlayerController()
+{
+	ConstructorHelpers::FClassFinder<UABHUDWidget> ABHUDWidgetRef(TEXT("/Game/ArenaBattle/UI/WBP_ABHUD.WBP_ABHUD_C"));
+	if (ABHUDWidgetRef.Class)
+	{
+		ABHUDWidgetClass = ABHUDWidgetRef.Class;
+	}
+}
 
 void AABPlayerController::BeginPlay()
 {
@@ -11,4 +21,12 @@ void AABPlayerController::BeginPlay()
 	// 플레이 중에 마우스 나타나게 하고 싶으면 Shift+F1
 	FInputModeGameOnly GameOnlyInputMode;
 	SetInputMode(GameOnlyInputMode);
+
+	// 게임이 시작되면 HUD 위젯 생성
+	ABHUDWidget = CreateWidget<class UABHUDWidget>(this, ABHUDWidgetClass);
+	if (ABHUDWidget)
+	{
+		// 뷰포트 화면에 출력하도록 호출
+		ABHUDWidget->AddToViewport();
+	}
 }
